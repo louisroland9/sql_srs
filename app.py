763d1b4c3cd0e3cfab33573ffdd1ab4151c64eb1
louis ROLAND <louisroland9@gmail.com>
2026-09-24 18:18:02 +0200

    add db

--- app.py
+++ app.py
@@ -1,52 +1,52 @@
 # pylint: disable=missing-module-docstring
+import ast
 import duckdb
 import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-ANSWER_STR = """
-select * from beverages
-cross join food_items
-"""
-
-#solution_df = duckdb.sql(ANSWER_STR).df()
-
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to review?",
-        ("cross_joins", "GroupBy", "Window Functions"),
+        ("cross_joins", "GroupBy", "window_functions"),
         index=None,
         placeholder="Select a theme",
     )
 
     st.write("You selected: ", theme)
 
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'")
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
+
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
 
-#     try:
-#         result = result[solution_df.columns]
-#         st.dataframe(result.compare(solution_df))
-#     except KeyError as e:
-#         st.write("Some columns are missing")
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
 
-# tab1, tab2 = st.tabs(["Tables", "Solution"])
+tab1, tab2 = st.tabs(["Tables", "Solution"])
 
-# with tab1:
-#     st.write("table: beverages")
-#     st.write(beverages)
-#     st.write("table: food_items")
-#     st.write(food_items)
-#     st.write("expected:")
-#     st.write(solution_df)
+with tab1:
+    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
 
 
-# with tab2:
-#     st.write(ANSWER_STR)
+with tab2:
+    st.write(answer)
+
