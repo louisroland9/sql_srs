91e809107d4b57145e88207f41828663c58cb767
louis ROLAND <louisroland9@gmail.com>
2026-09-24 15:55:11 +0200

    ajout d'une db d'exos

--- app.py
+++ app.py
@@ -1,68 +1,52 @@
 # pylint: disable=missing-module-docstring
-import io
-
 import duckdb
-import pandas as pd
 import streamlit as st
 
-CSV = """
-beverage,price
-Orange juice,2.5
-Expresso,2
-Tea,3
-"""
-
-beverages = pd.read_csv(io.StringIO(CSV))
-
-CSV2 = """
-food_item,food_price
-Cookie juice,2.5
-Chocolatine,2
-Muffin,3
-"""
-
-food_items = pd.read_csv(io.StringIO(CSV2))
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
 ANSWER_STR = """
 select * from beverages
 cross join food_items
 """
 
-solution_df = duckdb.sql(ANSWER_STR).df()
+#solution_df = duckdb.sql(ANSWER_STR).df()
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review?",
-        ("Joins", "GroupBy", "Window Functions"),
+        ("cross_joins", "GroupBy", "Window Functions"),
         index=None,
         placeholder="Select a theme",
     )
 
-    st.write("You selected: ", option)
+    st.write("You selected: ", theme)
+
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'")
+    st.write(exercise)
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
+# if query:
+#     result = duckdb.sql(query).df()
+#     st.dataframe(result)
 
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
+#     try:
+#         result = result[solution_df.columns]
+#         st.dataframe(result.compare(solution_df))
+#     except KeyError as e:
+#         st.write("Some columns are missing")
 
-tab1, tab2 = st.tabs(["Tables", "Solution"])
+# tab1, tab2 = st.tabs(["Tables", "Solution"])
 
-with tab1:
-    st.write("table: beverages")
-    st.write(beverages)
-    st.write("table: food_items")
-    st.write(food_items)
-    st.write("expected:")
-    st.write(solution_df)
+# with tab1:
+#     st.write("table: beverages")
+#     st.write(beverages)
+#     st.write("table: food_items")
+#     st.write(food_items)
+#     st.write("expected:")
+#     st.write(solution_df)
 
 
-with tab2:
-    st.write(ANSWER_STR)
+# with tab2:
+#     st.write(ANSWER_STR)
--- init_db.py
+++ init_db.py
@@ -5,6 +5,19 @@ import duckdb
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
 # ------------------------------------------------------------
+# EXERCISES LIST
+# ------------------------------------------------------------
+
+data = {
+    "theme": ["cross_joins", "window_functions"],
+    "exercise_name": ["beverages_and_food", "sizes_and_trademarks"],
+    "tables": [["beverages", "food_items"], ["sizes", "trademarks"]],
+    "last_reviewed": ["1980-01-01", "1970-01-01"],
+}
+memory_state_df = pd.DataFrame(data)
+con.execute("CREATE TABLE memory_state AS SELECT * FROM memory_state_df")
+
+# ------------------------------------------------------------
 # CROSS JOIN EXERCISES
 # ------------------------------------------------------------
 csv = """
